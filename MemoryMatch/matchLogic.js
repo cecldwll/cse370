663de202a78665d515f95e2f40297384b236a579
matthew morton <mor21018@byui.edu>
2025-10-01 12:15:32 -0600

    cards array/shuffle array

--- MemoryMatch/matchLogic.js
+++ MemoryMatch/matchLogic.js
@@ -13,12 +13,12 @@ async function MakeBoard(totalMatch){
 
 async function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
-    // Pick a random index from 0 to i
+
     const j = Math.floor(Math.random() * (i + 1));
 
-    // Swap elements at positions i and j
     [array[i], array[j]] = [array[j], array[i]];
     }
+    
     return array;
 }
 
