d6cabbfe0395adaed95c7a0caa8a0429f2ec7034
erschm240 <schmitze608@gmail.com>
2025-10-31 11:42:09 -0600

    Fixed the memory card grid, cleaned up some unnecessary and commented code in match.html, match.css, and matchVisual.js

--- MemoryMatch/matchVisual.js
+++ MemoryMatch/matchVisual.js
@@ -1,56 +1,7 @@
-// Card flip animation
+// Card flip animation (not used anymore, but keeping in comments just in case)
 
-const card = document.querySelector(".card");
+// const card = document.querySelector(".card");
 
-card.addEventListener("click", () => {
-    card.classList.toggle("flipped");
-});
-
-// .card {
-//     width: 100px;
-//     height: 100px;
-//     perspective: 1000px;
-// }
-// .card-inner {
-//     position: relative;
-//     width: 100%;
-//     height: 100%;
-//     text-align: center;
-//     transition: transform 0.8s;
-//     transform-style: preserve-3d;
-// }
-
-// .flip {
-//     transform: rotateY(180deg);
-// }
-
-// .card-front, .card-back {
-//     position: absolute;
-//     width: 100%;
-//     height: 100%;
-//     backface-visibility: hidden;
-// }
-
-// .card-front {
-//     background-color: gray;
-// }
-
-// .card-back {
-//     background-color: dodgerblue;
-//     transform: rotateY(180deg);
-// }
-
-// <div>
-//     <p>Temporary card flip testing div. Will be removed.</p>
-//     <div class="card">
-//         <div class="card-inner">
-//             <div class="card-front">
-//                 <p>F</p>
-//             </div>
-//             <div class="card-back">
-//                 <p>B</p>
-//             </div>
-//         </div>
-//     </div>
-// </div>
-// <br>
+// card.addEventListener("click", () => {
+//     card.classList.toggle("flipped");
+// });
