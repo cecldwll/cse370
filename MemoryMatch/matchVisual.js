a26c112a7827d53fba5840123a869178350742a3
erschm240 <schmitze608@gmail.com>
2025-11-07 20:41:33 -0700

    Removed unused code in matchVisual.js, made the home logo a link to the homepage.

--- MemoryMatch/matchVisual.js
+++
@@ -1,7 +0,0 @@
-// Card flip animation (not used anymore, but keeping in comments just in case)
-
-// const card = document.querySelector(".card");
-
-// card.addEventListener("click", () => {
-//     card.classList.toggle("flipped");
-// });
